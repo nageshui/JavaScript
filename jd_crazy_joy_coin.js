debd99bef2331b5b0c73307bebd1bd883e25f0ad
caojh <cjh@echase.cn>
2021-06-28 13:01:31 +0800

    1

--- jd_crazy_joy_coin.js
+++ jd_crazy_joy_coin.js
@@ -122,7 +122,7 @@ async function jdCrazyJoyNew()
   //await $.wait(1000)
 
   for (var loopcnt = 0; loopcnt < 33; loopcnt++) {
-    var startLevel= Math.max($.joyIds);
+    var startLevel= 33;
     console.log(startLevel)
     await recursiveMergeJoy(startLevel)
   }
